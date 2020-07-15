a9a719acc8faaa9007c0b727d2d1153ea3e6168e
Pierpaolo Follia <pfollia@gmail.com>
2020-07-15 20:12:27 +0200

    Fix export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,6 @@ import { ComelitVedoPlatform } from './comelit-vedo-platform';
 import { API } from 'homebridge';
 import { PLATFORM_NAME, PLUGIN_IDENTIFIER } from './constants';
 
-export default function(homebridge: API) {
-  homebridge.registerPlatform(PLUGIN_IDENTIFIER, PLATFORM_NAME, ComelitVedoPlatform);
-}
+export = (api: API) => {
+  api.registerPlatform(PLUGIN_IDENTIFIER, PLATFORM_NAME, ComelitVedoPlatform);
+};
